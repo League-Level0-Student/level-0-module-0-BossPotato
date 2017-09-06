0d1d42a3b00a61eec84f5cd07e049671d54b2d0f
chumister <alanchu4432@yahoo.com>
2017-09-05 18:27:03 -0700

    lvl0_day1

---
+++ src/EverythingIsAwesome.java
@@ -0,0 +1,22 @@
+import javax.swing.JOptionPane;
+
+public class EverythingIsAwesome {
+	// This recipe tells the user that what they like is awesome
+	//
+	// 1. Create a class called EverythingIsAwesome
+	
+	
+	// 2. Add a main method
+	public static void main(String[] args) {
+		
+	
+	
+	// 3. Use JOptionPane.showInputDialog to ask them what they like, e.g. What do you like?
+	String space = JOptionPane.showInputDialog("What do you like?");
+	// HINT: Use a variable to store their answer, e.g. space
+	
+	
+	// 4. Use JOptionPane.showMessageDialog to tell them it is awesome, e.g. space is awesome
+JOptionPane.showMessageDialog(null, space + " is awesome!");
+	}
+}
---
+++ src/RobotGraffiti.java
@@ -0,0 +1,43 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotGraffiti {
+	// This recipe draws the first letter of your name using the Robot
+	//
+	// 1. Create a class called RobotGraffiti
+	public static void main(String[] args) {
+		
+	// 2. Use the Robot to draw the first letter of your name
+	Robot a = new Robot();
+	a.penDown();
+	a.setSpeed(10);
+	// HINT: Use the RobotSquare recipe to help remember how to draw using Robot
+	a.setAngle(30);
+	a.move(200);
+	a.setAngle(150);
+	a.move(200);
+	a.setAngle(-30);
+	a.move(100);
+	a.setAngle(-90);
+	a.move(100);
+	a.hide();
+	
+	// 3. BONUS: Write your entire first name using the Robot
+	a.penUp();
+	a.setAngle(90);
+	a.move(250);
+	a.setAngle(180);
+	a.move(100);
+	a.setAngle(0);
+	a.penDown();
+	a.move(200);
+	a.setAngle(180);
+	a.move(100);
+	a.setAngle(45);
+	a.move(160);
+	a.setAngle(-135);
+	a.move(160);
+	a.turn(135);
+	a.move(160);
+	
+	}
+}
